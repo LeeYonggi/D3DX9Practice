6f141867045a0e77292e01b289b70798fa3804a7
LeeYonggi <35089435+LeeYonggi@users.noreply.github.com>
2019-05-12 22:26:23 +0900

    메인프로시저 테스트용 메시 삭제

--- D3DX9PRACTICE/MainProc.h
+++ D3DX9PRACTICE/MainProc.h
@@ -4,13 +4,7 @@ class MainProc
 public:
 	MainProc();
 	virtual ~MainProc();
-
-private:
-	Mesh *mesh;
-	Mesh *sphere;
-	D3DXVECTOR3 spherePosition;
-	MeshLoader *m_MeshLoader;
-
+	
 public:
 	void Init();
 	void Update();
